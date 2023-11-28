90e4ae99ebe4afce929b7655f14cfb56e35c5d55
GuidoJ <guidojanota@gmail.com>
2023-11-28 19:53:53 +0100

    Refactoring BranchController

--- src/main/java/project/controllers/BranchController.java
+++ src/main/java/project/controllers/BranchController.java
@@ -24,53 +24,82 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping ("/branch")
 public class BranchController {
-	
+
+	private static final String ATTRIBUTE_NAME_TITLE = "title";
+	private static final String ATTRIBUTE_NAME_BRANCHES = "branches";
+	private static final String ATTRIBUTE_NAME_SUCCESS = "success";
+	private static final String ATTRIBUTE_NAME_ERROR = "error";
+	private static final String SEARCH_ERROR_MESSAGE = "The data entered does not match any Branch in the system.";
+	private static final String URL_REDIRECT_BRANCH = "redirect:/branch";
+
 	@Autowired
 	private BranchService branchService;
 	
 	@Autowired
 	private CountryService countryService;
 	
-
 	@GetMapping("")
 	public String listBranches(Model model){
 		List<BranchDto> branches = branchService.getAllBranches();
-
-		model.addAttribute("title", "Available Branches");
-		model.addAttribute("branches", branches);
-
+		model.addAttribute(ATTRIBUTE_NAME_TITLE, "Available Branches");
+		model.addAttribute(ATTRIBUTE_NAME_BRANCHES, branches);
 		return "/views/branches/branches-list";
-	}
-	
-	@GetMapping("/add")
-	public String showNewBranchForm(Model model) {
-		BranchDto branchDto = new BranchDto();
-		List<Country> countries = countryService.listCountries();
-	
-		model.addAttribute("title", "Add Branch");
-		model.addAttribute("button", "Save Branch");
-		model.addAttribute("branch", branchDto);
-		model.addAttribute("countries",countries);
-
-		return "/views/branches/form-save-branch";
 	}
 
 	@GetMapping("/search")
 	public String searchBranch(@RequestParam("nameOrId") String nameOrId, Model model, RedirectAttributes msg) {
 		BranchDto branchDto = getBranchDto(nameOrId);
 		if (branchDto == null) {
-			msg.addFlashAttribute("error", "The data entered does not match any Branch in the system.");
-			return "redirect:/branch";
+			msg.addFlashAttribute(ATTRIBUTE_NAME_ERROR, SEARCH_ERROR_MESSAGE);
+			return URL_REDIRECT_BRANCH;
 		}
+		model.addAttribute(ATTRIBUTE_NAME_TITLE, "Branch found");
+		model.addAttribute(ATTRIBUTE_NAME_BRANCHES, branchDto);
+		model.addAttribute(ATTRIBUTE_NAME_SUCCESS, "Yes! We know that Branch.");
+		return "/views/branches/branches-list";
+	}
 
-		model.addAttribute("title", "Branch found");
-		model.addAttribute("branches", branchDto);
-		msg.addFlashAttribute("success", "Yes! We know that name.");
+	@GetMapping("/add")
+	public String showNewOrUpdateBranchForm(Model model) {
+		return showBranchForm(model, new BranchDto(), "Add Branch", "Save Branch");
+	}
 
-//		return "/views/branches/branches-list"; // no me muestra el "msg"
-		return "redirect:/branch"; // con esto me muestra el "msg" pero no el model
+	@PostMapping("/save")
+	public String saveBranch(@Valid @ModelAttribute BranchDto branchDto,
+							 BindingResult result, RedirectAttributes msg) {
+		//User input Validation
+		if (hasErrorsOrMissingCountry(branchDto, result)) {
+			msg.addFlashAttribute(ATTRIBUTE_NAME_ERROR, "Verify that all fields are complete.");
+			return getRedirectForBranchForm(branchDto);
+		}
+		//System Validation
+		if (!branchService.saveBranch(branchDto)) {
+			msg.addFlashAttribute(ATTRIBUTE_NAME_ERROR, "Name already taken.");
+			return getRedirectForBranchForm(branchDto);
+		}
+		msg.addFlashAttribute(ATTRIBUTE_NAME_SUCCESS, "Branch has been saved successfully");
+		return URL_REDIRECT_BRANCH;
+	}
 
+	@GetMapping("/edit/{id}")
+	public String editBranch(@PathVariable("id") Long id, Model model, RedirectAttributes msg) {
+		BranchDto branchDto = branchService.getOneBranchById(id);
+		if (branchDto == null) {
+			System.out.println("Branch not found");
+			msg.addFlashAttribute(ATTRIBUTE_NAME_ERROR, SEARCH_ERROR_MESSAGE);
+			return URL_REDIRECT_BRANCH;
+		}
+		return showBranchForm(model, branchDto, "Edit Branch", "Update Branch");
+	}
 
+	@GetMapping("/delete/{id}")
+	public String deleteBrunch(@PathVariable("id") Long id, RedirectAttributes msg) {
+		if (branchService.deleteBranchById(id)) {
+			msg.addFlashAttribute("warning", "Branch deleted.");
+		} else {
+			msg.addFlashAttribute(ATTRIBUTE_NAME_ERROR, SEARCH_ERROR_MESSAGE);
+		}
+		return URL_REDIRECT_BRANCH;
 	}
 
 	private BranchDto getBranchDto(String nameOrId) {
@@ -78,14 +107,12 @@ public class BranchController {
 		if (nameOrId.length() < 1) {
 			return null;
 		}
-
 		if (isNumeric(nameOrId)) {
 			Long id = Long.parseLong(nameOrId);
 			branchDto = branchService.getOneBranchById(id);
 		} else {
 			branchDto = branchService.getOneBranchByName(nameOrId);
 		}
-
 		return branchDto;
 	}
 
@@ -93,69 +120,27 @@ public class BranchController {
 		return str.chars().allMatch( Character::isDigit );
 	}
 
-	@PostMapping("/save")
-	public String saveBranch(@Valid @ModelAttribute BranchDto branchDto,
-							 BindingResult result, Model model, RedirectAttributes msg) {
-
-		//User input Validation
-		if (hasErrorsOrMissingCountry(branchDto, result)) {
-			handleErrors(branchDto, model, msg);
-			msg.addFlashAttribute("error", "Verify that all fields are complete.");
-			return "redirect:/branch/add";
-		}
-
-		//System Validation
-		if (!branchService.saveBranch(branchDto)) {
-			msg.addFlashAttribute("error", "Name already taken.");
-			return "redirect:/branch/add";
-		}
-
-		msg.addFlashAttribute("success", "Branch has been saved successfully");
-		return "redirect:/branch";
+	private String showBranchForm(Model model, BranchDto branchDto, String title, String button) {
+		List<Country> countries = countryService.listCountries();
+		model.addAttribute(ATTRIBUTE_NAME_TITLE, title);
+		model.addAttribute("button", button);
+		model.addAttribute("branch", branchDto);
+		model.addAttribute("countries", countries);
+		return "/views/branches/form-save-branch";
 	}
 
 	private boolean hasErrorsOrMissingCountry(BranchDto branchDto, BindingResult result) {
 		return result.hasErrors() || branchDto.getCountry() == null;
 	}
 
-	private void handleErrors(BranchDto branchDto, Model model, RedirectAttributes msg) {
-		List<Country> countries = countryService.listCountries();
-
-		model.addAttribute("title", "Add Branch");
-		model.addAttribute("button", "Save Branch");
-		model.addAttribute("branch", branchDto);
-		model.addAttribute("countries", countries);
-
+	private String getRedirectForBranchForm(BranchDto branchDto) {
+		return (branchDto.getId() != null) ? "redirect:/branch/edit/" + branchDto.getId() : "redirect:/branch/add";
 	}
 
-	@GetMapping("/edit/{id}")
-	public String editBranch(@PathVariable("id") Long id, Model model, RedirectAttributes msg) {
-		BranchDto branchDto = branchService.getOneBranchById(id);
 
-		if (branchDto == null) {
-			System.out.println("Branch not found");
-			msg.addFlashAttribute("error", "The data entered does not match any Branch in the system.");
-			return "redirect:/branch";
-		}
 
-		List<Country> countries = countryService.listCountries();
-		model.addAttribute("title", "Edit Branch");
-		model.addAttribute("button", "Update Branch");
-		model.addAttribute("branch", branchDto);
-		model.addAttribute("countries", countries);
-		return "/views/branches/form-save-branch";
-	}
 
-	@GetMapping("/delete/{id}")
-	public String deleteBrunch(@PathVariable("id") Long id, RedirectAttributes msg) {
 
-		if (branchService.deleteBranchById(id)) {
-			msg.addFlashAttribute("warning", "Branch deleted");
-		} else {
-			msg.addFlashAttribute("error", "The data entered does not match any Branch in the system.");
-		}
-		return "redirect:/branch";
-	}
 
 }
 
